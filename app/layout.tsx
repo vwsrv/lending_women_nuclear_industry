bc23dc00d568b253ef9dd497e010b5177a99559b
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-07-22 19:52:21 +0300

    refactor: refactor globals.css url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import '../src/app/styles/global.css'
+import '@/app/styles/globals.css'
 import { inter, montserrat } from '@/app/fonts/fonts'
 
 const RootLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
