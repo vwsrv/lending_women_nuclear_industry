22fea0037834fccb51532005ea92fa0dfe1e367b
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-07-26 22:32:51 +0500

    fix: edit color style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,19 +8,19 @@ const config: Config = {
   ],
   theme: {
     colors: {
-      'white': '#FFF',
+      white: '#FFF',
       'white-smoke': '#F7F7F7',
-      'indigo': '#2D2458',
-      'magenta': '#C410E1',
+      indigo: '#2D2458',
+      magenta: '#C410E1',
 
       'violet-pastel': '#5D4796', // + непрозрачность 23% / формула 1шт
-      'blue': '#2F00A9', // + непрозрачность 23% / формулы 3шт
+      blue: '#2F00A9', // + непрозрачность 23% / формулы 3шт
 
       'magenta-dark': '#823A8D', // первая кнопка "подписаться" и вторая кнопка "поддержать проект"
       'magenta-light': '#F6BDFF', // вторая кнопка "подписаться"
       'blue-midnight': '#1F088A', // первая кнопка "поддержать проект"
-      'lavender': '#D3CAFF', // кнопка "зарегистрироваться"
-      'violet-light': '#EAC7FF', // кнопка "подать заявку"
+      lavender: '#D3CAFF', // кнопка "зарегистрироваться"
+      'violet-light': '#EAC7FF' // кнопка "подать заявку"
     },
     extend: {
       backgroundImage: {
