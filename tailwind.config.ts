5c453b2455b0b1b269574dc24a30e9214da7a0a8
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-07-29 05:30:18 +0500

    fix: edit color styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,19 +8,30 @@ const config: Config = {
   ],
   theme: {
     colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
       white: 'rgba(var(--white-color), 1)',
-      'white-smoke': '#F7F7F7',
       indigo: '#2D2458',
-      magenta: '#C410E1',
-
-      'violet-pastel': '#5D4796', // + непрозрачность 23% / формула 1шт
-      blue: '#2F00A9', // + непрозрачность 23% / формулы 3шт
-
-      'magenta-dark': '#823A8D', // первая кнопка "подписаться" и вторая кнопка "поддержать проект"
-      'magenta-light': '#F6BDFF', // вторая кнопка "подписаться"
-      'blue-midnight': '#1F088A', // первая кнопка "поддержать проект"
-      lavender: '#D3CAFF', // кнопка "зарегистрироваться"
-      'violet-light': '#EAC7FF' // кнопка "подать заявку"
+      gray: {
+        DEFAULT: '#F7F7F7',
+        dark: '#B1B1B1'
+      },
+      violet: {
+        DEFAULT: '#906FCC', // Кнопки + "схемы хим.связей"
+        // pastel: '#5D4796', // + непрозрачность 23% / формула 1шт
+        // light: '#EAC7FF',  // кнопка "подать заявку"
+      }, 
+      blue: {
+        DEFAULT: '#0984CB', // Кнопки
+        // DEFAULT: '#2F00A9', // + непрозрачность 23% / формулы 3шт
+        // midnight: '#1F088A', // первая кнопка "поддержать проект"
+      }, 
+      magenta: {
+        DEFAULT: '#C410E1',
+        // dark: '#823A8D', // первая кнопка "подписаться" и вторая кнопка "поддержать проект"
+        // light: '#F6BDFF', // вторая кнопка "подписаться"
+      }, 
+      // lavender: '#D3CAFF', // кнопка "зарегистрироваться"
     },
     extend: {
       backgroundImage: {
