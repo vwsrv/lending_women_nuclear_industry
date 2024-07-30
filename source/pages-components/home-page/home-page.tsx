6f479269488625bd63d83879c73be46ef38881ce
JackSheepe <teterkinvklol@mail.ru>
2024-07-31 02:37:52 +0700

    fix: delete mock

--- source/pages-components/home-page/home-page.tsx
+++ source/pages-components/home-page/home-page.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/source/shared/ui/button'
-import { MissionAndTasks } from '@/source/widgets/missionAndTasks'
 
 export const HomePage: React.FC = () => {
   return (
@@ -7,18 +6,6 @@ export const HomePage: React.FC = () => {
       <Button className="bg-[#D3CAFF] rounded-2xl px-6 py-8 text-[#2E2559] bold text-2xl hover:opacity-75 transition-opacity">
         Зарегистрироваться
       </Button>
-      <MissionAndTasks
-        missions={[
-          'sss',
-          'формирование ролевых моделей для будущих женщин- ученых',
-          'sss',
-          'sss',
-          'sss',
-          'sss',
-          'sss'
-        ]}
-        tasks={['формирование ролевых моделей для будущих женщин- ученых']}
-      />
     </main>
   )
 }
