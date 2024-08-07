37ad3475d4ff5087cc15f494ed98257c60501383
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-08-08 01:27:34 +0500

    feat: remove mock data

--- source/pages-components/home-page/home-page.tsx
+++ source/pages-components/home-page/home-page.tsx
@@ -1,45 +1,4 @@
 import { Button } from '@/source/shared/ui/button'
-import { Trustees } from '@/source/shared/widgets/trustees'
-
-import img001 from '@/shared/ui/person-list/person-02.png'
-
-const data = [
-  {
-    photo: img001,
-    name: 'Юлия Рузанкина',
-    info: 'Ph.D международной докторской программы в науки KU Leuven (Бельгия) и Università Cattolica del Sacro Cuore (Италия)'
-  },
-  {
-    photo: img001,
-    name: 'Ирина Шрайбер',
-    info: 'кандидат физико-математических науки, учёный, исследователь, ментор, лектор, в разные годы научный сотрудник ЦЕНРН, Женева​'
-  },
-  {
-    photo: img001,
-    name: 'Екатерина Иваниловская-Абдурахманова',
-    info: 'официальный представитель, советник- председателя Международного Проектного Конгресса, Председатель Пермского Молодежного Центра'
-  },
-  {
-    photo: img001,
-    name: 'Юлия Рузанкина',
-    info: 'Ph.D международной докторской программы в науки KU Leuven (Бельгия) и Università Cattolica del Sacro Cuore (Италия)'
-  },
-  {
-    photo: img001,
-    name: 'Ирина Шрайбер',
-    info: 'кандидат физико-математических науки, учёный, исследователь, ментор, лектор, в разные годы научный сотрудник ЦЕНРН, Женева​'
-  },
-  {
-    photo: img001,
-    name: 'Ирина Шрайбер',
-    info: 'кандидат физико-математических науки, учёный, исследователь, ментор, лектор, в разные годы научный сотрудник ЦЕНРН, Женева​'
-  },
-  {
-    photo: img001,
-    name: 'Екатерина Иваниловская-Абдурахманова',
-    info: 'официальный представитель, советник- председателя Международного Проектного Конгресса, Председатель Пермского Молодежного Центра'
-  }
-]
 
 export const HomePage: React.FC = () => {
   return (
@@ -47,7 +6,6 @@ export const HomePage: React.FC = () => {
       <Button className="bg-[#D3CAFF] rounded-2xl px-6 py-8 text-[#2E2559] bold text-2xl hover:opacity-75 transition-opacity">
         Зарегистрироваться
       </Button>
-      <Trustees trusteesList={data} />
     </main>
   )
 }
