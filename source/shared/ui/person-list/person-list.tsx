0961c995b6f1380989ccbbb18a1b75e629e87302
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-08-08 01:32:16 +0500

    fix: editing PersonList component

--- source/shared/ui/person-list/person-list.tsx
+++ source/shared/ui/person-list/person-list.tsx
@@ -6,15 +6,13 @@ export const PersonList: React.FC<personListProps> = ({ listItems }) => {
     <ul className="grid grid-cols-2 gap-y-7 gap-x-28">
       {listItems.map((item, index) => {
         return (
-          <li key={index} className="leading-10 flex flex-row">
+          <li key={index} className="flex flex-row">
             <Image
               src={item.photo}
               alt={'Фото ' + item.name}
-              width={100}
-              height={100}
-              className="rounded-full mr-[22px] w-[118px] h-[118px]"
+              className="rounded-full mr-6 w-32 h-32"
             />
-            <div className="mt-3">
+            <div className="mt-3 leading-10 text-xl/10">
               <h3 className="uppercase font-bold">{item.name}</h3>
               <p>{item.info}</p>
             </div>
