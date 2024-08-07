6e2c8cd6f1d4797b3cac158602ff46e4d60cbc91
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-08-08 01:18:40 +0500

    feat: add board of trustees component with mock data

--- source/shared/widgets/trustees/trustees.tsx
+++ source/shared/widgets/trustees/trustees.tsx
@@ -3,8 +3,8 @@ import { PersonList } from '@/source/shared/ui/person-list/person-list'
 
 export const Trustees: React.FC<trusteesProps> = ({ trusteesList }) => {
   return (
-    <section className="bg-indigo p-20">
-      <h2 className="uppercase text-6xl/tight font-bold mb-20 text-gray">
+    <section className="p-20 text-indigo bg-logo-white bg-no-repeat bg-top-logo-white-1">
+      <h2 className="uppercase text-6xl/tight font-bold mb-20 pr-72">
         Наш попечительский совет
       </h2>
       <PersonList listItems={trusteesList} />
