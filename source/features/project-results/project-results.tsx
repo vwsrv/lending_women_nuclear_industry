35967fe9fb3efbaf45ca76b1237fe3f331f5ccda
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-08-08 12:10:24 +0300

    refactor: px => rem

--- source/features/project-results/project-results.tsx
+++ source/features/project-results/project-results.tsx
@@ -8,31 +8,19 @@ export const ProjectResults: React.FC<projectResultsProps> = ({
   img
 }) => {
   return (
-    <section className="relative pt-[79px] pb-[87px] px-[80px] bg-[#2e2559] text-[#F7F7F7] flex flex-col">
-      <h3 className="pb-[18px] text-[55px] leading-[66.56px] font-bold">
-        {title}
-      </h3>
-      <h5 className="pb-[74px] text-[40px] leading-[60px] font-bold">
-        {subtitle}
-      </h5>
-      <div className="w-full px-[94px] flex justify-center flex-wrap gap-[100px]">
+    <section className="relative p-20 bg-[#2e2559] text-[#F7F7F7] flex flex-col">
+      <h3 className="pb-5 text-6xl font-bold">{title}</h3>
+      <h5 className="pb-20 text-4xl font-bold">{subtitle}</h5>
+      <div className="w-full px-24 flex justify-center flex-wrap gap-24">
         {items &&
           items.map((item, index) => (
-            <div className="flex flex-col items-center " key={index}>
-              <span className="text-[53px] leading-[60px] font-bold">
-                {item.number}
-              </span>
-              <p className="text-[40px] leading-[60px] font-normal">
-                {item.text}
-              </p>
+            <div className="flex flex-col items-center" key={index}>
+              <span className="text-5xl font-bold">{item.number}</span>
+              <p className="text-4xl font-normal">{item.text}</p>
             </div>
           ))}
       </div>
-      <Image
-        className="absolute top-[61px] right-[78px] w-[205px] h-[199px]"
-        src={img}
-        alt=""
-      />
+      <Image className="absolute top-16 right-20 w-52 h-48" src={img} alt="" />
     </section>
   )
 }
