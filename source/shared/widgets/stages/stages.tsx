22dc98701f64f75f40d55038f257ac8eaea9cc71
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-08-08 18:34:54 +0500

    fix: intermediate saving of changes

--- source/shared/widgets/stages/stages.tsx
+++ source/shared/widgets/stages/stages.tsx
@@ -2,7 +2,7 @@ import { stagesProps } from './types'
 
 export const Stages: React.FC<stagesProps> = ({}) => {
   return (
-    <section className="p-20 pt-11 bg-gray text-indigo bg-logo-white bg-no-repeat bg-top-logo-white-2 bg-[length:218px_60px]">
+    <section className="p-20 pt-11 bg-gray text-indigo bg-logo-white bg-no-repeat bg-top-logo-white-1 bg-[length:218px_61px]">
       <h2 className="uppercase text-6xl/tight font-bold mb-20">Этапы премии</h2>
     </section>
   )
