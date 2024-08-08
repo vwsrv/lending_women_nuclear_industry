d1a59f6ae1746218f7a399bd0c290acf67dfba84
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-08-08 12:05:11 +0300

    fix: prittier

--- source/features/women-science-technology/women-science-technology.tsx
+++ source/features/women-science-technology/women-science-technology.tsx
@@ -9,15 +9,9 @@ export const WomenScienceTechnology: React.FC<womenScienceTechnologyProps> = ({
 }) => {
   return (
     <section className="relative flex flex-col py-20 px-20 text-[#2D2458]">
-      <h3 className="pb-7 text-6xl font-bold">
-        {title}
-      </h3>
-      <h5 className="pb-4 text-4xl font-bold">
-        {subtitle}
-      </h5>
-      <p className="max-w-4xl text-xl font-normal">
-        {paragraph}
-      </p>
+      <h3 className="pb-7 text-6xl font-bold">{title}</h3>
+      <h5 className="pb-4 text-4xl font-bold">{subtitle}</h5>
+      <p className="max-w-4xl text-xl font-normal">{paragraph}</p>
       <Image
         className="absolute bottom-20 right-20"
         src={img}
