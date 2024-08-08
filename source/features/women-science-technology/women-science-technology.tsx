49bb4dd06205241dfc3a1816ccb206f8d99c091b
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-08-08 12:04:18 +0300

    refactor: px => rem

--- source/features/women-science-technology/women-science-technology.tsx
+++ source/features/women-science-technology/women-science-technology.tsx
@@ -8,18 +8,18 @@ export const WomenScienceTechnology: React.FC<womenScienceTechnologyProps> = ({
   img
 }) => {
   return (
-    <section className="relative flex flex-col py-[76px] px-[80px] text-[#2D2458]">
-      <h3 className="pb-[28px] text-[55px] font-bold leading-[66.56px]">
+    <section className="relative flex flex-col py-20 px-20 text-[#2D2458]">
+      <h3 className="pb-7 text-6xl font-bold">
         {title}
       </h3>
-      <h5 className="pb-[15px] text-[36px] font-bold leading-[43.57px]">
+      <h5 className="pb-4 text-4xl font-bold">
         {subtitle}
       </h5>
-      <p className="max-w-[884px] text-[20px] font-normal leading-loose">
+      <p className="max-w-4xl text-xl font-normal">
         {paragraph}
       </p>
       <Image
-        className="absolute bottom-[80px] right-[80px]"
+        className="absolute bottom-20 right-20"
         src={img}
         alt=""
         placeholder="blur"
