788fbb482412608d8c05a4dff49a81914479b6f7
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2024-08-15 06:31:14 +0500

    feat: first version contacts-task

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,7 +27,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
         'poly-pattern': "url('/polygons.svg')",
-        'logo-white': "url('/logo-bg-white.svg')"
+        'logo-white': "url('/logo-bg-white.svg')",
+        'logo-dark': "url('/for-contacts/logo-bg-dark.svg')",
+        
+        'icons-email': "url('/for-contacts/email.svg')",
+        'icons-phone': "url('/for-contacts/phone.svg')",
+        'icons-whatsup': "url('/for-contacts/whatsup.svg')",
+        'icons-instagram': "url('/for-contacts/instagram.svg')",
+        'icons-website': "url('/for-contacts/website.svg')",
+        'icons-kolba': "url('/for-contacts/kolba.svg')",
+        'icons-vkontakte': "url('/for-contacts/vkontakte.svg')",
+        'icons-telegram': "url('/for-contacts/telegram.svg')",
       },
       backgroundPosition: {
         'top-logo-white-2': 'right 5rem top 5rem'
