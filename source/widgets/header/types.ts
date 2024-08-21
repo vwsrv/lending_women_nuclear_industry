b0cce36be3193775f05c5025ecd259e94afe09eb
Max Romanenko <pr@mxm.su>
2024-08-21 20:02:35 +0500

    Revert "Revert 15 feat/zhe 166/add components zhe 143 144 145 stages experts trustees"

--- source/widgets/header/types.ts
+++ source/widgets/header/types.ts
@@ -1,6 +1,11 @@
+import { StaticImageData } from 'next/image'
 import { HTMLAttributes, DetailedHTMLProps } from 'react'
 
 export interface headerProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+  titleImage: StaticImageData
+  cloudMoleculeImage: StaticImageData
+  womenImage: StaticImageData
   description: string
+  colbaImage: StaticImageData
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,9 @@ const config: Config = {
       violet: '#906FCC',
       magenta: '#C410E1'
     },
+    fontSize: {
+      h3: '55px'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -32,9 +35,6 @@ const config: Config = {
       backgroundPosition: {
         'top-logo-white-1': 'right 5rem top 2.75rem',
         'top-logo-white-2': 'right 5rem top 5rem'
-      },
-      fontSize: {
-        h3: '55px'
       }
     }
   },
